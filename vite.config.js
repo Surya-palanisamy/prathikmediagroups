5801535fa924b40b0693fd1224f2b63ecca8d8a3
Surya-palanisamy <suryasitit@gmail.com>
2024-11-01 22:01:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Change 'build' to your desired folder name
+    outDir: "build", 
   },
 });
-// vite.config.js
+
